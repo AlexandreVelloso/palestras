869e8bff2ab394dc7b7d15a93eb2a97ad75e6d17
Alexandre Velloso <xande.velloso@gmail.com>
2019-06-17 23:12:40 -0300

    Click event

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,26 +1,35 @@
 <template>
-    <div class="item">
-      <div class="card">
-        <div class="card-body">
-          <h5 class="card-title"> {{ title }} </h5>
-          
-          <a v-bind:class="{ 'btn btn-success': isDone , 'btn btn-danger': !isDone }" href="#">
-            <div v-if="isDone">Done</div>
-            <div v-if="!isDone">Not finished</div>
-          </a>
+  <div class="item">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">{{ title }}</h5>
 
-        </div>
+        <a
+          v-bind:class="{ 'btn btn-success': isDone , 'btn btn-danger': !isDone }"
+          v-on:click="toggleButton"
+          href="#"
+        >
+          <div v-if="isDone">Done</div>
+          <div v-if="!isDone">Not finished</div>
+        </a>
+        
       </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'TodoItem',
+  name: "TodoItem",
   props: {
     id: Number,
     title: String,
     isDone: Boolean
+  },
+  methods: {
+    toggleButton: function() {
+      this.isDone = !this.isDone;
+    }
   }
-}
+};
 </script>
